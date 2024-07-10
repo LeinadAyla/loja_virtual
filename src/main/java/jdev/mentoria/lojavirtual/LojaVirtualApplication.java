00f008888284ca4e5687c5d06d0d9aa8b5f20538
LeynadAyla <wilsonfreitashgem@gmail.com>
2024-07-10 13:01:49 -0300

    Consegui as propriedades de anotações do Pool de conexões - Hikari e demais necessárias para preparar a aplicação para os testes de persistencias no Postman, acrescentei ajustes no arquivo pom.xml entre outros detalhes atinentes a programação...

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualApplication.java
@@ -3,14 +3,22 @@ package jdev.mentoria.lojavirtual;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 import jakarta.persistence.Entity;
 
 @SpringBootApplication
-@EntityScan(basePackages = "jjdev.mentoria.lojavirtual.model")
+@EntityScan(basePackages = "jdev.mentoria.lojavirtual.model")
 @ComponentScan(basePackages = {"jdev.*"})
 @EnableJpaRepositories(basePackages = {"jdev.mentoria.lojavirtual.repository"})
 @EnableTransactionManagement
@@ -19,5 +27,18 @@ public class LojaVirtualApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualApplication.class, args);
 	}
+	
+	@Configuration
+	public class UserDatasourceConfiguration {
+
+	    @Primary
+	    @Bean(name="userProperties")
+	    @ConfigurationProperties("spring.datasource")
+	    public DataSourceProperties dataSourceProperties() {
+
+	        return new DataSourceProperties();
+	    }
+	}
+	
 
 }
