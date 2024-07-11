8886dea24e41bae61dae5445de5d5c64b2063ce1
LeynadAyla <wilsonfreitashgem@gmail.com>
2024-07-10 22:42:14 -0300

    Aula 3.1 meu primeiro endpoint com as classes de testes fechando também com o controller.

--- src/test/java/jdev/mentoria/lojavirtual/LojaVirtualApplicationTests.java
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualApplicationTests.java
@@ -1,13 +1,36 @@
 package jdev.mentoria.lojavirtual;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LojaVirtualApplicationTests {
+import jdev.mentoria.lojavirtual.controller.AcessoController;
+import jdev.mentoria.lojavirtual.model.Acesso;
+import jdev.mentoria.lojavirtual.repository.AcessoRepository;
+import jdev.mentoria.lojavirtual.service.AcessoService;
+
+@SpringBootTest(classes = LojaVirtualApplication.class)
+public class LojaVirtualApplicationTests {
+	
+//	@Autowired
+//	private AcessoService  acessoService;
+	
+//	@Autowired
+//	private AcessoRepository acessoRepository;
+	
+	@Autowired
+	private AcessoController acessoController;  
 
 	@Test
-	void contextLoads() {
+	public void testCadastraAcesso() {
+		
+		Acesso acesso = new Acesso();
+		
+		acesso.setDescricao("ROLE_ADMIN");
+		
+		acessoController.salvarAcesso(acesso);
+		
+		
 	}
 
 }
